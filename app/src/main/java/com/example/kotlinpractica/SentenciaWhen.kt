8b33a52814784426e5b2a65f03bb6d4cbe052015
Luispmv <luispmv07@hotmail.com>
2024-10-30 15:13:49 -0600

    Ejercicios en Kt: sentencia when booleana

--- app/src/main/java/com/example/kotlinpractica/SentenciaWhen.kt
+++ app/src/main/java/com/example/kotlinpractica/SentenciaWhen.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinpractica
 
 fun main(){
-    println(diasIdiomas("Tuesday"))
+//    println(diasIdiomas("Tuesday"))
+//    println(edades(20))
+    println(vehiculos("car"))
 }
 
 
@@ -41,10 +43,46 @@ fun diasIdiomas(dias:String){
 //Clasificación de edades
 //Escribe una función que, con base en la edad de una persona,
 // devuelva si es un niño, adolescente, adulto o anciano.
+fun edades(edad:Int){
+    when(edad){
+        in 1..12->{
+            println("Niño")
+        }
+        in 13..17->{
+            println("Adolescente")
+        }
+        in 18..64->{
+            println("Adulto")
+        }
+
+        in 65 .. 120->{
+            println("Adulto Mayor")
+        }
+        else->{
+            println("No se que seas")
+        }
+    }
+}
+
 
 //Categorías de vehículos
 //Dada la categoría de un vehículo ("car", "truck", "motorcycle"),
 // devuelve un tipo específico de licencia requerido ("A", "B", "C").
+
+fun vehiculos(vehiculo:String){
+    when{
+        vehiculo == "car"->{
+            println("A")
+        }
+        vehiculo == "truck"->{
+            println("B")
+        }
+        vehiculo == "motorcycle"->{
+            println("C")
+        }
+    }
+}
+
 
 //Meses del año
 //Dado el número de un mes (1 para enero, 2 para febrero, etc.), devuelve el nombre del mes usando match-case.
